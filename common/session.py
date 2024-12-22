1b5e8b100a9fb0584cdb34cfef6e841682a38085
West Xu <xu-lai-xi@live.com>
2024-12-22 11:55:29 +0800

    openai role "system" -> "developer"

--- common/session.py
+++ common/session.py
@@ -22,7 +22,7 @@ class Session(object):
         """
         session = Session.all_sessions.get(context.session_id, [])
         if len(session) == 0:
-            system_item = {"role": "system", "content": context.system_prompt}
+            system_item = {"role": "developer", "content": context.system_prompt}
             session.append(system_item)
             Session.all_sessions[context.session_id] = session
         user_item = {"role": "user", "content": context.query}
